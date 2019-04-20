e888bb34fc1ad9ab3b092b59099675cd6e376b9a
me <49545028+parismaris@users.noreply.github.com>
2019-04-20 16:54:19 -0400

    fix-flowErrorsAndFlowNotSeeingFileChanges

--- src/comp/Elem/Elem.js
+++ src/comp/Elem/Elem.js
@@ -3,12 +3,12 @@
 import React from 'react'
 import s from './Elem.css'
 
-type Pops = {
+type Props = {
   title: string,
   body: number,
 }
 
-const Elem = (props: Props) => (
+ const Elem = (props: Props) => (
   <div className={s.comp} >
     <span>{props.title}</span> <span className={s.message}>{'test-message'}</span> <span>{props.body}</span> <span className={s.end}>hello</span>
   </div>
